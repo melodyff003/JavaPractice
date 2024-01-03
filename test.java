e2ac8a2b9665023f60b1578f51caed6c92722a7e
Melody Flores <melody03@hotmail.com.ar>
2024-01-03 18:36:09 -0300

    first commit

---
+++ test.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args){
+        System.out.println("Testing!");
+
+        //scanner 
+        Scanner ingreso = new Scanner(System.in);
+        System.out.println("Enter your name, please /n");
+        String name = ingreso.next();
+
+    }
+    
+}
