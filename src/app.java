5a7949502f1484d2de21b520ea986296661b0f0e
Melody Flores <melody03@hotmail.com.ar>
2024-01-10 00:43:17 -0300

    First practice excercises

--- src/app.java
+++ src/app.java
@@ -1,5 +1,52 @@
 package src;
 
+import java.util.Scanner;
+
 public class app {
-    
+   public static void main(String[] args) {
+        System.out.println("Ejercicio 1");
+
+      //   String name = "Melody";
+      //   String surname = "Flores";
+
+      //   System.out.println("Mi nombre es " + name + " " + surname);
+
+   System.out.println("Ejercicio 2");
+
+   int x, y, z; 
+
+   Scanner number = new Scanner(System.in);
+   System.out.println("Enter a number");
+   
+   x = number.nextInt();
+   System.out.println("The number is: " + x);
+
+   System.out.println("Enter a number");
+   
+   y = number.nextInt();
+   System.out.println("The number is: " + y);
+
+   System.out.println("Enter a number");
+   
+   z = number.nextInt();
+   System.out.println("The number is: " + z);
+
+   if(x > y){
+      if(x > z){
+         System.out.println("El valor más grande es el de: " + x);
+      }else{
+         System.out.println("El valor de " + x + " es más grande que el de " + y + " pero más chico que el de " + z);
+      }
+   }else if(x < y){
+      if(x )
+   }
+
+   System.out.println("Ejercicio 3");
+   System.out.println("Ejercicio 4");
+   System.out.println("Ejercicio 5");
+   System.out.println("Ejercicio 6");
+
+      
+               
+   }
 }
